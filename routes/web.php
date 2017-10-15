9d40d545e4d3dc71becbbcfc7c1c29a5ccf447fe
kellsland <kellsland3@gmail.com>
2017-10-16 12:19:07 +1300

    About page *test*
    /about

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/about', function () {
+    return '<h1>About</h1>';
+});
 
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
