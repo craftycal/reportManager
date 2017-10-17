e796b008cebe70d560634a1e851bacf9a4bca01a
Callum Powell <callum.powell.1@gmail.com>
2017-10-17 17:05:40 +1300

    stylein
    got style g~!

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,13 +2,12 @@
 <html>
 <head>
   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Report Manager</title>
   <link rel="shortcut icon" type="image/png" href="..\resources\assets\img\RM_small.png">
   <link rel="stylesheet" href="css/app.css">
   <link href="https://fonts.googleapis.com/css?family=Kreon|Muli" rel="stylesheet">
-  <script type="text/javascript" src="js/app.js">
-    
-  </script>
+  <script type="text/javascript" src="js/app.js"></script>
   
 </head>
 <body>
